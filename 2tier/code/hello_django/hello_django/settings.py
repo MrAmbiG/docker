085c1d373bb3b9dddb5c85b89ea6b4f4e9ed442b
Gajendra D Ambi <gajuambi@gmail.com>
2019-01-06 02:32:22 +0530

    separate docker files

--- 2tier/code/hello_django/hello_django/settings.py
+++ 2tier/code/hello_django/hello_django/settings.py
@@ -73,23 +73,15 @@ WSGI_APPLICATION = 'hello_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'HOST': 'db', # set in docker-compose.yml
-#         'PORT': 5432 # default postgres port
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db', # set in docker-compose.yml
+        'PORT': 5432 # default postgres port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
