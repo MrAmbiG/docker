58d278539d068982c10178a83fd28e1dbae32031
Gajendra D Ambi <gajuambi@gmail.com>
2019-01-10 14:25:54 +0530

    static file, media file location updated in settings.py

--- 4tier/django/code/hello/hello/settings.py
+++ 4tier/django/code/hello/hello/settings.py
@@ -127,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/mediafiles/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
