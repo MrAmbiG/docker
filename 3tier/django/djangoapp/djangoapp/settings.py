c1aa4b881e91ea3f9540b964b894fcc3fe680008
Gajendra D Ambi <gajuambi@gmail.com>
2019-01-12 18:37:24 +0530

    nginx testing

--- 3tier/django/djangoapp/djangoapp/settings.py
+++ 3tier/django/djangoapp/djangoapp/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# as declared in NginX conf, it must match /opt/services/djangoapp/static/
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'static')
+
+# do the same for media files, it must match /opt/services/djangoapp/media/
+MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'media')
